6c5700b4783ab2749e5256ee4485543da01a88ef
unheardof77 <morgan.tolman04@gmail.com>
2025-06-16 18:19:38 -0400

    Added Allvideos page.

--- client/src/components/VideoRender.tsx
+++ client/src/components/VideoRender.tsx
@@ -2,14 +2,11 @@
 
 
 export default function VideoRender({ vidStr }: { vidStr: string }) {
-    console.log(vidStr);
     const buf = Buffer.from(vidStr, 'base64');
     const blob = new Blob([buf], { type: 'video/mp4' });
     const videoUrl = URL.createObjectURL(blob);
     return (
         <div className="bg-white shadow-md rounded-lg p-6 max-w-3xl mx-auto mt-8">
-            <h1 className="text-2xl font-semibold text-gray-800 mb-3">Video Render</h1>
-            <p className="text-gray-600 mb-6">This component is for rendering video content.</p>
             <video
                 controls
                 width="100%"
